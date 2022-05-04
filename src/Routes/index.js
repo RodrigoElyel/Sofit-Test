f6301f7922db0c5ea3b89a1e0c29d70cbaf36e59
rodrigoelyel <rodrigoelyelcb@gmail.com>
2022-05-04 19:31:22 -0300

    Configs

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -7,16 +7,23 @@ import { createStackNavigator } from '@react-navigation/stack';
 // SCREENS
 import HomeScreen from '../Screens/Home'
 import DetailsScreen from '../Screens/Details'
+import LoginScreen from '../Screens/Login'
+
+import colors from '../Styles/colors';
 
 const Stack = createStackNavigator();
 
 const StackScreen = () => {
     return (
         <Stack.Navigator
-            screenOptions={{
-                headerShown: true,
-            }}
+        screenOptions={{
+            headerShown: true,
+            headerTintColor: colors.black,
+            headerStyle: { backgroundColor: colors.green },
+            headerTitleAlign: 'center',
+        }}
         >
+            <Stack.Screen name="Login" component={LoginScreen} />
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="Details" component={DetailsScreen} />
         </Stack.Navigator>
