89c712ad9dc7fe2b24605d707b68ce9b13c732bb
rodrigoelyel <rodrigoelyelcb@gmail.com>
2022-05-04 19:29:53 -0300

    Adicionando objeto de cores

---
+++ src/Styles/colors.js
@@ -0,0 +1,18 @@
+export default {
+    green: "#4BCD00",
+    red: "#8D0801",
+    blue: "#072AC8",
+    gold: "#FFB700",
+    white: "#fff",
+    black: '#000',
+    gray: '#888888',
+    azulClarinho: "#90e0ef",
+    escuro: "#2f3640",  
+    medium: '#6e6969',
+    lightgray: '#f8f8f4',
+    danger: '#ff5252',
+    amarelo: "#f0932b",
+    vermelho: "#ba181b",
+    laranja: '#FF4500',
+    laranja2: '#FF6A33'
+}
