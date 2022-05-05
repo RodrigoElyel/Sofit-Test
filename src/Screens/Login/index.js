8b5cb9b905c6ef1e3db8bc33860344aae20c7ef6
rodrigoelyel <rodrigoelyelcb@gmail.com>
2022-05-05 11:52:12 -0300

    Configs

--- src/Screens/Login/index.js
+++ src/Screens/Login/index.js
@@ -1,28 +1,87 @@
-import { View, Text } from 'react-native'
+import { View, Text, StatusBar } from 'react-native'
 import React from 'react'
 
 // Components
 import Input from '../../Components/Input'
 import Screen from '../../Components/Screen'
+import Button from '../../Components/Button'
+
+// API
+import { login } from '../../Api/Auth'
+
+// ALERTS
+import { showMessage, hideMessage } from "react-native-flash-message";
+
+import colors from '../../Styles/colors'
 
 
 
 const LoginScreen = () => {
 
-  const [email, setEmail] = React.useState('');
+  const [email, setEmail] = React.useState('')
 
   console.log(email)
 
+  const autheticate = async () => {
+
+    const response = await login(email)
+
+    if (response) {
+      showMessage({
+        message: "Sucesso",
+        description: "Login efetuado!",
+        type: "success",
+        statusBarHeight: StatusBar.currentHeight,
+        floating: true,
+      });
+
+      await AsyncStorage.setItem('@storage:user', JSON.stringify(response))
+    } else {
+      showMessage({
+        message: "Falha",
+        description: "Não foi possível realizar o login. Verifique sua conexão!",
+        type: "danger",
+        statusBarHeight: StatusBar.currentHeight,
+        floating: true,
+      });
+    }
+
+  }
+
+  const submit = () => {
+
+    console.log(!email.length)
+
+    if (!email.length) {
+      showMessage({
+        message: "Falha",
+        description: "Insira um email válido!",
+        type: "danger",
+        statusBarHeight: StatusBar.currentHeight,
+        floating: true,
+      });
+    } else {
+      autheticate()
+    }
+    
+  }
 
   return (
-    <View>
+    <Screen style={{justifyContent: 'center'}}>
       <Input
-        styleContainer={{ marginTop: 8 }}
+        styleContainer={{ marginTop: 8, width: '90%', alignSelf: 'center' }}
         placeholder="email"
         value={email}
         onChange={value => setEmail(value)}
+        keybordType="email-address"
       />
-    </View>
+      <Button
+        styleContainer={{ marginTop: 8, width: '90%', alignSelf: 'center', backgroundColor: colors.gold }}
+        label="Entrar"
+        onPress={() => submit()}
+      />
+
+    </Screen>
   )
 }
 
